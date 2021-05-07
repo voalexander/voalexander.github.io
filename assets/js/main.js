9e22ce6b013b9234f5e326e5591f97280cbe0f01
voalexander <alexhovo@gmail.com>
2021-05-07 04:27:25 -0500

    Remove dropdown bar on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,35 +46,6 @@
 	// Scrolly.
 		$('.scrolly').scrolly();
 
-	// Nav.
-
-		// Button.
-			$(
-				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
 	// Carousels.
 		$('.carousel').each(function() {
 
